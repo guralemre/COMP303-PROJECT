ffc2ef2a182ce7fbe34ca3cc55e4d991e70942a2
guralemre <emregural@windowslive.com>
2024-12-05 23:42:12 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Bu projeyi çalıştırmak için öncelikle requirements.txt içindeki librarylerin "pip install -r requirements.txt --user" komutu ile yüklenmesi gerekiyor.
+Projenin çalıştırılması için terminalden "python main.py" komutu yazılmalı.
+  
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+#react ile flask arasındaki bağlantı
+app = Flask(__name__)
+CORS(app)  # React ile Flask arasında CORS sorunu yaşamamak için
+
+@app.route('/api/hello', methods=['GET'])
+def hello_world():
+    return jsonify(message="Connected from flask!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
